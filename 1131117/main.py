fc1c72e12dc0ca4d878c6295a2ec9bda51a2013c
蕭雅恪 <yako0918124051@gmail.com>
2024-11-17 10:47:48 +0800

    1111

--- 1131117/main.py
+++ 1131117/main.py
@@ -1,2 +1,18 @@
-from dotenv import dotenv_values
 
+from dotenv import load_dotenv
+import streamlit as st
+from time import sleep
+from datasource import get_data
+load_dotenv()
+
+placeholder = st.empty()
+with placeholder:
+    with st.spinner('下載資料中...'):
+        get_data()
+
+    st.success("下載完成",icon=":material/favorite:")
+    sleep(3)
+
+
+placeholder.empty()
+    
