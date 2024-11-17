ee96e6e8dd72a8a9cd6cf9d6f58f61647754801d
蕭雅恪 <yako0918124051@gmail.com>
2024-11-17 11:17:25 +0800

    1111

--- 1131117/main.py
+++ 1131117/main.py
@@ -19,13 +19,30 @@ placeholder.empty()
 """
 
 
-
+"""
 from dotenv import load_dotenv
 import streamlit as st
 from time import sleep
 from datasource import get_data
 load_dotenv()
 
+
+@st.dialog("目前發生問題,請稍後再試")
+def alert():
+        st.write('連線有問題')
+        st.stop()
+
+with st.spinner('下載資料中...'):
+        get_data()
+alert()
+st.write('顯示頁面')
+"""
+
+from dotenv import load_dotenv
+import streamlit as st
+from time import sleep
+from tools.datasource import get_data
+load_dotenv()
 
 @st.dialog("目前發生問題,請稍後再試")
 def alert():
